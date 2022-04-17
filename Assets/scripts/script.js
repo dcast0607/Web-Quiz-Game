5c0b7a8b5b92944688bbc12d02e87f1293943b51
Daniel Castro <dcast0607@gmail.com>
2022-04-16 17:27:10 -0700

    Mostle final HTML/CSS Structure

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,9 +1,26 @@
-var startButton = document.getElementById("startButton");
-
-
 function startGame() {
-    window.location.href = "quiz-content.html";
-    console.log("start");
+    $("#welcomeMessageBox").addClass("hidden");
+    $("#startButton").addClass("hidden");
+    $("#countDownTimerBox").removeClass("hidden");
+    $("#startButtonContainer").addClass("hidden");
+    var startTimer = 10;
+    setInterval (function(){
+        if (startTimer > 0) {
+            startTimer--;
+            $("#secondsLeftStart").text(startTimer);
+            console.log(startTimer);
+        }
+
+        else {
+            $("#quizContentBox").removeClass("hidden");
+            $("#nextButton").removeClass("hidden");
+            $("#countDownTimerBox").addClass("hidden");
+            $("#nextButtonContainer").removeClass("hidden");
+            clearInterval();
+        }
+
+    }, 1000)
+
 }
 
 startButton.addEventListener("click", startGame);
